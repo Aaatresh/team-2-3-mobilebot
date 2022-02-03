375700b6760df57e0905ea2b153d188458b0e540
aaatresh <aaatresh@umich.edu>
2022-02-03 14:10:45 -0500

    latest plots

--- python/plot_vels_square.py
+++ python/plot_vels_square.py
@@ -7,7 +7,7 @@ parser.add_argument("--log_file", type=str, default=None)
 
 args = parser.parse_args()
 
-my_data = np.genfromtxt(args.log_file, delimiter=',', skip_header=1)
+my_data = np.genfromtxt(args.log_file, delimiter=',', skip_header=1)[:270]
 
 #print(my_data)
 b = 0.1584
@@ -15,29 +15,29 @@ b = 0.1584
 X = [x * 0.1 for x in range(1, my_data.shape[0]+1)]
 
 plt.figure()
-plt.title("Plot of robot's x position against time at 0.25m/s for 2s.")
-plt.ylabel("x position (m)")
+plt.title("Plot of robot forward velocity when completing a square once at 0.25m/s and pi/4 rad/s.")
+plt.ylabel("Forward velocity (m/s)")
 plt.xlabel("Time (s)")
 plt.plot(X, my_data[:, 6], label="Low pass filtered reference signal")
-plt.plot(X, (my_data[:, 8] + my_data[:, 9]) / 2.0, label="Robot's x position from odometry")
+plt.plot(X, (my_data[:, 8] + my_data[:, 9]) / 2.0, label="Robot forward velocity from odometry")
 axes = plt.gca()
 #axes.set_aspect("equal")
 plt.legend(loc="upper left")
 plt.show()
 
-"""
+
 plt.figure()
-plt.title("Plot of robot frame heading against time when turned at pi/8 rad/s for 2s.")
-plt.ylabel("Robot frame heading (rad)")
+plt.title("Plot of robot rotational velocity when completing a square once at 0.25m/s and pi/4 rad/s.")
+plt.ylabel("Rotational velocity (rad/s)")
 plt.xlabel("Time (s)")
 #plt.plot(X, my_data[:, 7], label="Low pass filtered reference signal")
 plt.plot(X, my_data[:, 7], label="Reference signal")
-plt.plot(X, (-my_data[:, 8] + my_data[:, 9]) / b, label="Robot frame heading from odometry")
+plt.plot(X, (-my_data[:, 8] + my_data[:, 9]) / b, label="Robot rotational velocity from odometry")
 axes = plt.gca()
 #axes.set_aspect("equal")
 plt.legend(loc="upper left")
 plt.show()
-"""
+
 # plt.savefig("path.png")
 
 
