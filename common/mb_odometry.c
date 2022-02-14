7c6bc0aa77e5cc9e03a0e1edf2cd5d1bf05f99c9
Devansh Ramgopal Agrawal <devansh@umich.edu>
2022-02-14 14:03:17 -0500

    edit

--- common/mb_odometry.c
+++ common/mb_odometry.c
@@ -91,6 +91,7 @@ void mb_update_odometry(mb_odometry_t* mb_odometry, mb_state_t* mb_state){
 	odo_msg.theta = mb_odometry->theta;
 	odo_msg.utime = now;
 
+	printf("PUBLISHING ODOM WITH TIME %i", odo_msg.utime);
 	odometry_t_publish(lcm, ODOMETRY_CHANNEL, &odo_msg);
 }
 
